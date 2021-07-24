bf2798d64e2dec0a480cb5f00a2db6bb5301d564
James Macbox1 <ninjachimpstudios@gmail.com>
2021-07-24 15:09:03 +1000

    problems with the debounce

--- TripleStirrer/TripleStirrer.ino
+++ TripleStirrer/TripleStirrer.ino
@@ -14,12 +14,14 @@ const int STATE_TIMER_ACTIVE = 1;
 const int STATE_OVERRIDE_ON = 2;
 const int MAXVAL = 255;
 const unsigned long DEBOUNCEDELAY = 50; 
+const unsigned long DEBUGDELAY = 10000;
 // vars
-int BUTTONSTATE[] = {0,0,0};
-int BUTTON1STATELAST[] = {0,0,0};
+int BUTTONSTATE[] = {HIGH,HIGH,HIGH};
+int BUTTONSTATELAST[] = {HIGH,HIGH,HIGH};
 int LEDSTATE[] = {LOW,LOW,LOW};
 int CHANNELSTATE[] = {STATE_TIMER_WAIT,STATE_TIMER_WAIT,STATE_TIMER_WAIT};
 unsigned long LASTDEBOUNCE[] = {0,0,0};
+unsigned long LASTDEBUG = 0;
 
 // ###
 
@@ -33,7 +35,9 @@ void setup() {
   // initialize I/O
   for (int i=0; i<CHANNELS; i++) {
     pinMode(LEDPIN[i], OUTPUT);
-    pinMode(BUTTONPIN[i], INPUT);
+    // enables the 5v pullup resistor so we don't need it externally
+    // note that a press will go LOW - open read HIGH
+    pinMode(BUTTONPIN[i], INPUT_PULLUP); 
     pinMode(FANPIN[i], OUTPUT);
     digitalWrite(LEDPIN[i],LOW);
     analogWrite(FANPIN[i],0);
@@ -49,7 +53,17 @@ void setup() {
 }
 
 void loop() { 
- 
+  setLeds();
+  checkButtons();
+  setLedStates();
+  doDebug();
+}
+
+void setLedStates() {
+  for (int i=0; i<CHANNELS; i++) {
+    // simple for now, we'll do blinking later
+    LEDSTATE[i] = (CHANNELSTATE[i] == STATE_OVERRIDE_ON) ? HIGH : LOW;
+  }
 }
 
 void setLeds() {
@@ -60,6 +74,45 @@ void setLeds() {
 
 void checkButtons() {
   for (int i=0; i<CHANNELS; i++) {
+    int reading = !digitalRead(BUTTONPIN[i]); // inverted for HIGH default
+    if (reading != BUTTONSTATELAST[i]) { // is there a button state change?
+      LASTDEBOUNCE[i] = millis(); // reset the debounce timer
+    }
+    if ((millis() - LASTDEBOUNCE[i]) > DEBOUNCEDELAY) { // we'll only enter here if the state has been stable for > DEBOUNCEDELAY
+      Serial.print(F("press "));
+      Serial.println(i);
+      if (reading != BUTTONSTATE[i]) {
+        BUTTONSTATE[i] = reading;
+        Serial.print(F("Button "));
+        Serial.print(i);
+        Serial.print(F("state "));
+        Serial.println(BUTTONSTATE[i]);
+        if (BUTTONSTATE[i] == LOW) {
+          CHANNELSTATE[i] = (CHANNELSTATE[i] == STATE_OVERRIDE_ON) ? STATE_TIMER_WAIT : STATE_OVERRIDE_ON;
+        }
+      }
+    }
+  }
+}
+
+void doDebug() {
+  if (millis() > DEBUGDELAY && ((millis() - DEBUGDELAY) > LASTDEBUG)) {
+    Serial.println(F("#############"));
+    for (int i=0; i<CHANNELS; i++) {
+      Serial.print(F("Channel State "));
+      Serial.print(i);
+      Serial.print(F(":"));
+      Serial.println(CHANNELSTATE[i]);
+      Serial.print(F("Button State "));
+      Serial.print(i);
+      Serial.print(F(":"));
+      Serial.println(BUTTONSTATE[i]);
+      Serial.print(F("Button Read State "));
+      Serial.print(i);
+      Serial.print(F(":"));
+      Serial.println(digitalRead(BUTTONPIN[i]));
+    }
     
+    LASTDEBUG = millis();
   }
 }

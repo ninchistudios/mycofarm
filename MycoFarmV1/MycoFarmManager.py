e717a7f6ef6ae3ac23c5be5c4a1dd28890955de7
James pi3b04 <ninjachimpstudios@gmail.com>
2021-10-12 13:49:55 +1000

    working static feeds to AIO

--- MycoFarmV1/MycoFarmManager.py
+++ MycoFarmV1/MycoFarmManager.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
+# https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5
+# https://github.com/adafruit/Adafruit_IO_Python/blob/master/examples/api/data.py
+
+import os
 import serial
+from Adafruit_IO import Client, Feed, Data, RequestError
+from decouple import config
+import datetime
+
 if __name__ == '__main__':
+    ADAFRUIT_IO_KEY = config('AIOKEY')
+    ADAFRUIT_IO_USERNAME = config('AIOUSER')
+    aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
+    try:
+        mtemp = aio.feeds('mtemp')
+    except RequestError:
+        tfeed = Feed(name="mtemp")
+        mtemp = aio.create_feed(tfeed)
+    try:
+        mhumi = aio.feeds('mhumi')
+    except RequestError:
+        hfeed = Feed(name="mhumi")
+        mhumi = aio.create_feed(hfeed)
+
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+            aio.append(mtemp.key,24)
+            aio.append(mhumi.key,75)
